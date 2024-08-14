9a8025325a9075a3ff261c2200cf532c4c0f7d70
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-08-13 18:40:53 -0600

    Mejorando la salida de mensajes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,18 +9,18 @@ const login = async (req, res) => {
     const connection = getConnection();
     const [rows] = await connection.query('SELECT * FROM usuarios WHERE usuario = ?', [usuario]);
     if (rows.length === 0) {
-      return res.status(401).json({ message: 'Usuario no encontrado' });
+      return res.status(401).json({ message: 'Datos de acceso incorrectos' });
     }
     const user = rows[0];
     const passwordMatch = await bcrypt.compare(contrasena, user.contrasena);
 
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
+      return res.status(401).json({ message: 'Datos de acceso incorrectos' });
     }
 
     const token = jwt.sign({ userId: user.id }, 'secreto', { expiresIn: '1h' });
 
-    res.status(200).json({ token });
+    res.status(200).json({ message: 'Accesando', token});
 
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
