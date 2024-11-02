f2e58237994a60992235fdbeadb90801371679f2
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-11-01 18:00:45 -0600

    Se agregó la opcion de agregar eventos sin ser administrador, restructuracion del codigo y mejora de estilos más responsivos

--- src/app/components/TablaEventoNuevo.vue
+++ src/app/components/TablaEventoNuevo.vue
@@ -1,104 +1,100 @@
 <template>
-  <table class="eventoInformacion">
-    <div class="contenidoTabla">
+  <div class="pantalla">
+  <table class="contenedorTabla">
+    <div class="contenedorFilas">
+      <div style="width: 90%;">
+        <tr>
+          <td class="columnaEtiqueta"><label for="titulo">Evento:</label></td>
+          <td class="columnaInformacion"><input type="text" id="titulo" name="titulo" v-model="titulo" /></td>
+        </tr>
+        <tr>
+          <td class="columnaEtiqueta"><label for="institucion">Institución:</label></td>
+          <td class="columnaInformacion"><input type="text" id="institucion" name="institucion" v-model="institucion" />
+          </td>
+        </tr>
+        <tr>
+          <td><div class="columnaEtiqueta"><label for="fecha">Fecha:</label></div></td>
+          <td class="contenedorFechas columnaInformacion">
+            <div class="contenedorFecha"><input type="date" id="fecha" name="fecha" v-model="fecha" /></div>
+            <div class="contenedorFecha">
+              <div><label for="fechaFin">Fecha de<br>finalización:</label></div>
+              <div><input type="date" id="fechaFin" name="fechaFin" v-model="fechaFin" :disabled="!mostrarFechaFin" /></div>
+            </div>
+          </td>        
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <input type="checkbox" id="toggleFechaFin" v-model="mostrarFechaFin" />
+            <label for="toggleFechaFin">¿Agregar fecha de finalización?</label>
+          </td>
+        </tr>
+        <tr>
+          <td class="columnaEtiqueta"><label for="hora">Hora:</label></td>
+          <td><input type="time" id="hora" name="hora" v-model="hora" /></td>
+        </tr>
+        <tr>
+          <td class="columnaEtiqueta"><label for="lugar">Lugar:</label></td>
+          <td>
+            <div class="contenedorLugar">
+              <label><input type="checkbox" value="Auditorio" v-model="lugaresSeleccionados" /> Auditorio</label>
+              <label><input type="checkbox" value="Aula Magna" v-model="lugaresSeleccionados" /> Aula Magna</label>
+              <label><input type="checkbox" value="Aulas de usos múltiples" v-model="lugaresSeleccionados" /> Aulas de usos múltiples</label>
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="columnaEtiqueta"><label>Tipo de evento:</label></td>
+          <td class="contenedorEvento">
+            <div>
+              <input type="radio" id="privado" value="privado" v-model="tipoEvento" />
+              <label for="privado">Privado</label>
+            </div>
+            <div>
+              <input type="radio" id="publico" value="publico" v-model="tipoEvento" />
+              <label for="publico">Público</label>
+            </div>
+          </td>
+        </tr>
+        
       <tr>
-        <td class="columnaTitulo"><label for="titulo">Titulo:</label></td>
-        <td class="columnaContenido"><input type="text" id="titulo" name="titulo" v-model="titulo" /></td>
-      </tr>
-      <tr>
-        <td class="columnaTitulo"><label for="institucion">Institución:</label></td>
-        <td class="columnaContenido"><input type="text" id="institucion" name="institucion" v-model="institucion" />
-        </td>
-      </tr>
-      <tr>
-        <td class="columnaTitulo"><label for="fecha">Fecha:</label></td>
-        <td class="columnaContenido"><input type="date" id="fecha" name="fecha" v-model="fecha" class="pequeno" /></td>
-      </tr>
+          <td class="columnaEtiqueta"><label for="detalles">Detalles:</label></td>
+          <td class="columnaInformacion">
+            <div class="contenedorDetalles">
+              <label for="detalles" class="detalles-label">.</label>
+              <textarea id="detalles" name="detalles" v-model="detalles"></textarea>
+            </div>
+          </td>
+        </tr>
 
+        <tr>
+          <td class="columnaEtiqueta"><label for="imagen">Imagen:</label></td>
+          <td class="columnaContenido"><input type="file" id="imagen" @change="onFileChange('img', $event)" accept=".jpg, .jpeg, .png, .gif, .svg, .webp, .bmp" class="input-file" /></td>
+        </tr>
+        <tr>
+          <td class="columnaEtiqueta"><label for="pdf">PDF:</label></td>
+          <td class="columnaContenido"><input type="file" id="pdf" @change="onFileChange('pdf', $event)" accept=".pdf" class="input-file" /></td>
+        </tr>
 
-      <tr>
-        <td class="columnaTitulo"></td>
-        <td class="columnaContenido">
-          <input type="checkbox" id="toggleFechaFin" v-model="mostrarFechaFin" />
-          <label for="toggleFechaFin">¿Agregar fecha de finalización?</label>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="columnaTitulo"><label for="fechaFin">Fecha de finalización:</label></td>
-        <td class="columnaContenido"><input type="date" id="fechaFin" name="fechaFin" v-model="fechaFin" :disabled="!mostrarFechaFin" class="pequeno" /></td>
-      </tr>
-
-
-
-
-      <tr>
-        <td class="columnaTitulo"><label for="hora">Hora:</label></td>
-        <td class="columnaContenido"><input type="time" id="hora" name="hora" v-model="hora" class="pequeno" /></td>
-      </tr>
-
-
-      <tr>
-        <td class="columnaTitulo"><label for="lugar">Lugar:</label></td>
-        <td class="columnaContenido">
-          <div>
-            <label><input type="checkbox" value="Auditorio" v-model="lugaresSeleccionados" /> Auditorio</label>
-            <label><input type="checkbox" value="Aula Magna" v-model="lugaresSeleccionados" /> Aula Magna</label>
-            <label><input type="checkbox" value="Aulas de usos múltiples" v-model="lugaresSeleccionados" /> Aulas de usos múltiples</label>
-          </div>
-        </td>
-      </tr>
-
-
-
-
-
-      <tr>
-        <td class="columnaTitulo"><label for="estatus">Estatus:</label></td>
-        <td class="columnaContenido">
-          <select id="estatus" name="estatus" v-model="estatus" class="pequeno">
-            <option value="pendiente">Pendiente</option>
-            <option value="aceptado">Aceptado</option>
-          </select>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="columnaTitulo"><label for="tipoEvento">Tipo de evento:</label></td>
-        <td class="columnaContenido">
-          <input type="radio" id="privado" value="privado" v-model="tipoEvento" />
-          <label for="privado">Privado</label>
-          <input type="radio" id="publico" value="publico" v-model="tipoEvento" />
-          <label for="publico">Público</label>
-        </td>
-      </tr>
-
-
-
-
-
-
-      <tr>
-        <td class="columnaTitulo"><label for="detalles">Detalles:</label></td>
-        <div class="borde">
-          <td class="columnaContenido"><textarea id="detalles" name="detalles" v-model="detalles"></textarea></td>
-        </div>
-      </tr>
-      <tr>
-        <td class="columnaTitulo"><label for="imagen">Imagen:</label></td>
-        <td class="columnaContenido"><input type="file" id="imagen" @change="onFileChange('img', $event)" /></td>
-      </tr>
-      <tr>
-        <td class="columnaTitulo"><label for="pdf">PDF:</label></td>
-        <td class="columnaContenido"><input type="file" id="pdf" @change="onFileChange('pdf', $event)" /></td>
-      </tr>
+        <tr v-if="admin">
+          <td class="columnaEtiqueta"><label for="estatus">Estatus:</label></td>
+          <td>
+            <select id="estatus" name="estatus" v-model="estatus">
+              <option value="pendiente">Pendiente</option>
+              <option value="aceptado">Aceptado</option>
+            </select>
+          </td>
+        </tr>
+      </div>
     </div>
   </table>
   <div class="boton-container">
     <BotonConfirmar :titulo="titulo" :institucion="institucion" :fecha="fecha" :hora="hora" :lugar="lugaresSeleccionados.join(', ')"
       :detalles="detalles" :img="img" :pdf="pdf" :fechaFin="mostrarFechaFin ? fechaFin : null" 
-      :tipoEvento="tipoEvento" :estatus="estatus"/>
+      :tipoEvento="tipoEvento" :estatus="estatus" />
   </div>
+</div>
 </template>
 
 <script setup>
@@ -118,6 +114,7 @@ const fechaFin = ref(null);
 const mostrarFechaFin = ref(false);
 const estatus = ref('pendiente');
 const tipoEvento = ref('publico');
+const admin = localStorage.getItem('credencial');
 
 
 const onFileChange = (type, event) => {
@@ -127,96 +124,103 @@ const onFileChange = (type, event) => {
 };
 </script>
 
+
 <style scoped>
-.eventoInformacion {
-  min-width: 600px;
-
-  width: 100%;
-  height: 800px;
-  padding: 2% 50px 5% 15%;
+.pantalla{
+  display: flex;
+  gap: 30px;
+  flex-direction: column;
+  align-items: center;
+}
+.contenedorTabla{
+  min-width: 358px;
+  width: 70vw;
+  border-spacing: 0 15px;
   background-color: rgb(250, 211, 210);
-  border-radius: 7%;
-}
-
-.eventoInformacion tr {
-  height: 50px;
-}
-
-.columnaTitulo {
-  text-align: right;
-  font-size: 18px;
-  vertical-align: top;
-}
-
-.columnaTitulo label {
-  font-size: 18px;
-  font-weight: 600;
-
-}
-
-.columnaContenido {
-  padding-left: 5%;
-  text-align: left;
-  width: 100%;
-}
-
-.columnaContenido input {
-  font-size: 18px;
-  font-weight: 400;
-  width: 100%;
-  height: 30px;
-  border: none;
-  background-color: rgb(246, 246, 246);
-}
-
-.columnaContenido input.pequeno {
-  width: auto;
-  max-width: 200px;
-}
-
-.titulo {
-  font-size: 25px;
-  font-weight: 600;
-}
-
-.institucion {
-  font-size: 25px;
-  font-weight: 600;
-}
-
-.fechaLugar {
-  font-size: 19px;
-  font-weight: 500;
-}
-
-.borde {
-  background-color: rgb(246, 246, 246);
-  border-radius: 7%;
-  max-height: 600px;
-  min-height: 400px;
-  overflow: auto;
-  padding: 0 5px;
-}
-
-.detalles {
-  font-size: 18px;
-  line-height: 2;
-  text-align: justify;
-  height: 500px;
-}
-
-textarea {
-  width: 440%;
-  height: 400px;
-  margin: 10px 0;
-  resize: none;
-  background-color: rgb(246, 246, 246);
-  border-radius: 2%;
-}
-
-.boton-container {
   display: flex;
   justify-content: center;
-  margin-top: 20px;
+  border-radius: 40px;
 }
+.contenedorFilas{
+  margin: 20px;
+  width: 80%;
+  min-width: 320px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+}
+.contenedorFechas{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  .contenedorFecha{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center
+  }
+}
+.contenedorLugar{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  min-width: 190px;
+}
+
+.contenedorEvento{
+  label{
+    margin-left: 5px;
+  }
+}
+
+.columnaEtiqueta{
+  text-align: right;
+  font-size: 18px;
+  font-weight: 600;
+  display: flex;
+  justify-content: end;
+  margin-right: 20px;
+}
+
+textarea{
+  background-color: rgb(246, 246, 246);
+  min-width: 190px;
+  width: 100%;
+  min-height: 110px;
+  max-height: 500px;
+  resize: vertical;
+  padding: 10px;
+  border-radius: 15px;
+}
+
+.columnaInformacion {
+
+  width: 100%;
+  min-width: 190px;
+  input{
+    width: 100%;
+    min-width: 190px;
+    height: 30px;
+    border-radius: 2px;
+    border: none;
+    background-color: rgb(246, 246, 246);
+    font-size: 17px;
+    font-weight: 400;
+  }
+}
+
+.input-file {
+  min-width: 190px;
+  width: 100%;
+  height: 30px;
+}
+
+.detalles-label{
+  visibility:hidden;
+}
+
+div tr:nth-child(odd){
+  background-color: rgb(252, 221, 220);
+}
+
 </style>
