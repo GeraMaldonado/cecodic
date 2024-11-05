52250481f2de40b5fc6f771ea24d6919d4076910
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-11-05 15:14:45 -0600

    Se regresó el tipo de exportacion anterior para evitar fallos con el backend y solo se cambio la extencion del archivo de configuraicon de vite

---
+++ vite.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['calendar-month', 'calendar-date', 'calendar-range'].includes(tag),
+        }
+      }
+    })     
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src/app'
+    }
+  },
+  build: {
+    outDir: 'src/public'
+  }
+});
