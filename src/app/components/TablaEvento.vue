0e122c723319c4d85e48dfe2d9c2d46474ca7799
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-11-05 21:29:53 -0600

    Se agregó un mejor manejo de vistas para pantallas pequeñas

--- src/app/components/TablaEvento.vue
+++ src/app/components/TablaEvento.vue
@@ -8,8 +8,8 @@
         </td>
         <td class="columnaInformacion">
           <div class="tituloImagenWrapper">
-            <p class="titulo">{{ titulo }}</p>
             <img v-if="img" :src="rutaArchivo(img)" alt="Imagen del Evento" class="eventoImagen" />
+            <p class="titulo">{{ titulo }}</p>
           </div>
         </td>
       </tr>
@@ -50,7 +50,7 @@
         </td>
       </tr>
 
-      <tr>
+      <tr v-if="tipoEvento">
         <td class="columnaEtiqueta">
           <h3>Tipo de evento: </h3>
         </td>
@@ -59,7 +59,7 @@
         </td>
       </tr>
 
-      <tr>
+      <tr v-if="detalles">
         <td class="columnaEtiqueta">
           <h3>Detalles: </h3>
         </td>
@@ -81,8 +81,6 @@
 
 <script setup>
 import { fechaLarga, urlUpload } from './utils';
-import BotonEditar from './buttons/BotonEditar.vue';
-import BotonEliminar from './buttons/BotonEliminar.vue';
 
 const props = defineProps({
   titulo: String,
@@ -100,101 +98,90 @@ const props = defineProps({
 const rutaArchivo = (nombre) => `${urlUpload}/${nombre}`
 </script>
 
-<style>
+<style scoped>
 .detalles {
   font-size: 18px;
   line-height: 1.5;
-
   white-space: pre-line;
-  background-color: red;
+  background-color: white;
   padding: 10px;
   border-radius: 20px;
-}</style>
-<!--
-<style scoped>
-.eventoInformacion {
-  width: 100%;
-  height: 800px;
-  padding: 2% 50px 5% 15%;
-  background-color: rgb(250, 211, 210);
-  border-radius: 7%;
-}
-
-.eventoInformacion tr td {
-  padding-top: 0px;
-  padding-bottom: 0px;
-}
-
-.columnaTitulo {
-  text-align: right;
-  font-size: 18px;
-  vertical-align: top;
-}
-
-.columnaContenido {
-  padding-left: 5%;
-  text-align: left;
-  width: 100%;
-}
-
-.titulo {
-  font-size: 25px;
-  font-weight: 600;
-}
-
-.institucion {
-  font-size: 25px;
-  font-weight: 600;
-}
-
-.fechaLugar {
-  font-size: 19px;
-  font-weight: 500;
-}
-
-.borde {
-  background-color: rgb(246, 246, 246);
-  border-radius: 7%;
-  max-height: 600px;
-  min-height: 300px;
-  overflow: auto;
-  padding: 0 5px;
-}
-
-.detalles {
-  font-size: 18px;
-  line-height: 2;
-  text-align: justify;
-  height: 500px;
-  white-space: pre-line;
 }
 
 .tituloImagenWrapper {
   position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  padding-right: 80px;
+}
+
+.tituloImagenWrapper.con-imagen .titulo {
+  margin-right: 220px;
+}
+
+.titulo {
+  z-index: 1;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: left;
 }
 
 .eventoImagen {
   width: 250px;
   height: 250px;
+  object-fit: cover;  
   border-radius: 50%;
   border: 7px solid rgb(246, 246, 246);
+  background-color: rgb(246, 246, 246);
   position: absolute;
-  top: 50px;
-  right: 50px;
-  transform: translate(50%, -50%);
+  top: -100px;
+  right: -180px;
+  z-index: 0;
+  overflow: hidden;
 }
 
-.titulo {
-  margin-right: 270px;
-  z-index: 1;
+.contenedorTabla {
   position: relative;
+  overflow: visible;
 }
 
-.centrar {
-  text-align: center;
-  vertical-align: middle;
+@media (max-width: 900px) {
+  .tituloImagenWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-right: 0;
+  }
+
+  .eventoImagen {
+    position: static;
+    margin-bottom: 10px;
+    width: 180px;
+    height: 180px;
+  }
+
+  .titulo {
+    margin-right: 0;
+    text-align: center;
+  }
 }
-</style>-->
+
+@media (max-width: 740px) {
+  .contenedorFilas tr {
+    display: flex;
+    flex-direction: column;
+    align-content: center
+  }
+
+
+  .columnaInformacion {
+    display: flex;
+    justify-content: center;
+  }
+  .columnaEtiqueta{
+    h3{
+      width: 100%;
+      text-align: center;
+    }
+  }
+
+}
+</style>
