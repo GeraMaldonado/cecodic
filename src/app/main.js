43ad10b44a0dcdfa1eed9af5a00fdec3cce2a3aa
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-11-08 20:07:11 -0600

    Integracion del componente Toast para las notificaciones

--- src/app/main.js
+++ src/app/main.js
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App)
 app.use(router)
+app.use(PrimeVue);
+app.use(ToastService);
 app.component('VueDatePicker', VueDatePicker);
 app.mount('#app')
