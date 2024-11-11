fb2012407dc4751c30453cf723b3b5c6278fc6aa
Gera Maldonado <gmaldonadofelix@gmail.com>
2024-11-11 02:50:51 -0600

    Linea de estilos para el datepicker

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import 'primevue/resources/themes/saga-blue/theme.css';
